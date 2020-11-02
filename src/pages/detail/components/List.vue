3d7bcd190a94bb4f3417ec71173fda62552bcddc
yanghui23 <LBJ23_yanghui@163.com>
2020-11-02 11:03:36 +0800

    use recursive

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div
+      class="item"
+      v-for="(item, index) of list"
+      :key="index"
+    >
+      <div class="item-title border-bottom">
+        <span class="item-title-icon iconfont">&#xe625;</span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    .item-title-icon
+      font-size: .26rem
+      color: #f4ea2a
+  .item-children
+    padding: 0 .2rem
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
     </div>
     <router-link to="/city">
       <div class="header-right">{{this.city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
+        <span class="iconfont arrow-icon">&#xe60b;</span>
       </div>
     </router-link>
   </div>
